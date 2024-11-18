f23515a25b98f36a71110a75607b5367d90abf1b
Niranjan Kumar <niranjan882684@gmail.com>
2024-11-18 22:23:11 +0530

    first commit

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://df122038393256714352e813a6ad5a22@o4508319057641472.ingest.us.sentry.io/4508319061770240",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,51 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const config: Config = {
+  darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./sanity/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      screens: {
+        xs: "475px",
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: {
+          "100": "#E6F4FF",
+          DEFAULT: "#0066CC",
+        },
+        secondary: "#FFB800",
+        black: {
+          "100": "#4A4A4A",
+          "200": "#2D2D2D",
+          "300": "#6B7280",
+          DEFAULT: "#000000",
+        },
+        white: {
+          "100": "#F9FAFB",
+          DEFAULT: "#FFFFFF",
+        },
+      },
+      fontFamily: {
+        "work-sans": ["var(--font-work-sans)"],
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      boxShadow: {
+        100: "2px 2px 0px 0px rgb(0, 0, 0)",
+        200: "2px 2px 0px 2px rgb(0, 0, 0)",
+        300: "2px 2px 0px 2px rgb(0, 102, 204)",
       },
     },
   },
-  plugins: [],
-} satisfies Config;
+  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+};
+
+export default config;
